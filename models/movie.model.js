3cab4e25dec7d9597179e454b2c18ac892b3beaa
Nkugavarathan <kugavarathan28@gmail.com>
2025-07-26 00:07:55 +0530

    data validation added

--- models/movie.model.js
+++ models/movie.model.js
@@ -1,6 +1,10 @@
 import { model, Schema } from "mongoose"
 
-const schema = new Schema({ title: String, desc: String })
+const schema = new Schema({
+  //validation
+  title: { type: String, required: true, unique: true },
+  desc: { type: String, required: true, unique: true },
+})
 
 //create model
 
